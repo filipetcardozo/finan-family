13bef5e21e8d5df32a123976054f46dff1f17ee0
Filipe Cardozo <filipetcardozo@gmail.com>
2023-02-06 01:22:15 -0300

    fix: import iconbutton com bug

--- src/pages/invoice-entries/index.tsx
+++ src/pages/invoice-entries/index.tsx
@@ -1,29 +1,26 @@
+import React, { useState } from 'react'
 import Head from 'next/head'
-import { useState } from 'react'
-import { LayoutMobile } from '../../components/app-layout'
-import { useProtectPage } from '../../hooks/auth/useAuth'
-import { useInvoices } from '../../hooks/useInvoices'
-import { IInvoice } from '../../providers/invoices/types'
-import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
-import { Box } from '@mui/material'
-import dayjs from 'dayjs'
-import Typography from '@mui/material/Typography';
-import { formatterCurrency } from '../../utils/formatters'
-import IconButton from '@mui/material/IconButton/IconButton'
+import { LayoutMobile } from '../../components/app-layout';
+import { useProtectPage } from '../../hooks/auth/useAuth';
+import { useInvoices } from '../../hooks/useInvoices';
+import { IInvoice } from '../../providers/invoices/types';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import { Box } from '@mui/material';
+import dayjs from 'dayjs';
+import { formatterCurrency } from '../../utils/formatters';
+import IconButton from '@mui/material/IconButton';
 import BorderColorIcon from '@mui/icons-material/BorderColorOutlined';
-import Tooltip from '@mui/material/Tooltip'
+import Tooltip from '@mui/material/Tooltip';
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
-import { AddInvoiceModal } from '../../components/modal-addInvoice'
+import { AddInvoiceModal } from '../../components/modal-addInvoice';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import Button from '@mui/material/Button';
-import { useSnackbar } from 'notistack'
+import { useSnackbar } from 'notistack';
 
 export default function InvoiceEntries() {
-  useProtectPage()
+  useProtectPage();
 
   const { enqueueSnackbar } = useSnackbar();
   const { invoices, handleUpdateInvoice, handleDeleteInvoice } = useInvoices();
@@ -43,7 +40,7 @@ export default function InvoiceEntries() {
 
   const handleCloseModalDeleteInvoice = () => {
     setOpenModalDeleteInvoice({ invoiceId: '', open: false })
-  }
+  };
 
   const columns: GridColDef[] = [
     {
