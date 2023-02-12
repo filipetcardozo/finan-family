30ffe006e5fa0dca5c8a3dedd398bd8e6b1b3533
Filipe Cardozo <filipetcardozo@gmail.com>
2023-02-12 11:28:27 -0300

    refactor: ajustado as legendas do pie chart home page

--- src/components/charts/pieChart.tsx
+++ src/components/charts/pieChart.tsx
@@ -1,7 +1,12 @@
+import { List, Stack } from '@mui/material'
 import { useMemo } from 'react'
 import { PieChart as PieChartRecharts, ResponsiveContainer, Pie, Cell, Legend } from 'recharts'
 import { IInvoice } from '../../providers/invoices/types'
 import { formatterCurrency } from '../../utils/formatters'
+import Box from '@mui/material/Box';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import DonutLargeIcon from '@mui/icons-material/DonutLarge';
 
 interface Props {
   expenses: IInvoice[]
@@ -60,37 +65,34 @@ export const PieChart: React.FC<Props> = ({
 
   if (!dataPieChart) return <></>;
 
-  return <ResponsiveContainer width='96%' height='100%'>
-    <PieChartRecharts
-      width={400} height={400}
-    >
-      <Legend iconSize={15}
-        formatter={(value, payload: any) => <text
-          style={{
-            fontSize: 11,
-
-          }}
+  return <Stack flexDirection='row' width={1}>
+    <Box sx={{ width: '200px', maxHeight: '200px', overflowY: 'auto' }}>
+      <List dense>
+        {dataPieChart.map((v, index) => {
+          return <ListItem disablePadding key={v.value} style={{ color: COLORS[index % COLORS.length] }}>
+            <DonutLargeIcon style={{ marginRight: 7, marginLeft: 15, fontSize: 12, top: 1, position: 'relative' }} />
+            <ListItemText primary={<span style={{ fontSize: 12 }}>{v.expenseCategory}: {formatterCurrency(v.value)}</span>} />
+          </ListItem>
+        })}
+      </List>
+    </Box>
+    <ResponsiveContainer width='50%' height='100%'>
+      <PieChartRecharts margin={{ right: 0 }}>
+        <Pie
+          data={dataPieChart}
+          dataKey='value'
+          nameKey='expenseCategory'
+          cx='50%' cy='50%'
+          label={renderCustomizedLabel}
+          textAnchor=''
+          legendType='line'
+          labelLine={false}
         >
-          {value}: {formatterCurrency(payload.payload.value)}
-        </text>}
-        layout='vertical'
-        verticalAlign='middle'
-        align='left'
-      />
-      <Pie
-        data={dataPieChart}
-        dataKey='value'
-        nameKey='expenseCategory'
-        cx='50%' cy='50%'
-        label={renderCustomizedLabel}
-        textAnchor=''
-        legendType='line'
-        labelLine={false}
-      >
-        {expenses.map((entry, index) => (
-          <Cell stroke={COLORS[index % COLORS.length]} key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-        ))}
-      </Pie>
-    </PieChartRecharts>
-  </ResponsiveContainer>
+          {expenses.map((entry, index) => (
+            <Cell stroke={COLORS[index % COLORS.length]} key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+          ))}
+        </Pie>
+      </PieChartRecharts>
+    </ResponsiveContainer>
+  </Stack >
 }
